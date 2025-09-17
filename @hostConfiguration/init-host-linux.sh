0a77f1de1202de182487a3ea3d737ccb2f672ea2
Nicolas Fonrose <nicolas.fonrose@teevity.com>
2025-09-17 02:29:29 +0200

    Fix the Dockerization, HTTPS serving and OpenAPI description serving

---
+++ @hostConfiguration/init-host-linux.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Step000 - Check that expected variables are defined or exit right away with an help message with example values
+#
+#   export PRT_OPT_BASEPATH=/opt/prt/prt-videodownloader
+#   export PRT_VIDEODOWNLOAD_PROJECT=/home/teevity/Dev/0.perso/tee-webdavserver-goodnotesbackup
+#
+if [ -z "${PRT_OPT_BASEPATH}" ] || [ -z "${PRT_VIDEODOWNLOAD_PROJECT}" ]; then
+  echo -e "\nEnvironment variable not defined for PRT_OPT_BASEPATH=[${PRT_OPT_BASEPATH}] or PRT_VIDEODOWNLOAD_PROJECT=[${PRT_VIDEODOWNLOAD_PROJECT}].\n"
+  echo -e "  Example:"
+  echo -e "    export PRT_VIDEODOWNLOAD_PROJECT=/home/teevity/Dev/0.perso/prt-videodownloader"
+  echo -e "    export PRT_OPT_BASEPATH=/opt/prt/prt-videodownloader\n"
+  exit 1
+fi
+
+# Step001 - Create folder structure
+echo -e "\nStep001 - Creating the  directory structure... ([${PRT_OPT_BASEPATH}/opt/prt])"
